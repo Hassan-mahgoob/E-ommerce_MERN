b3a3cc0facf62b92e9724983014c1afa83da4cf0
Hassan-mahgoob <hassanmahgoob73@gmail.com>
2025-11-06 17:43:05 +0400

    Cart Page layout

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography } from "@mui/material";
+import { Box, ButtonGroup, Container, Button, Typography } from "@mui/material";
 import { useCart } from "../context/Cart/CartContext";
 const CartPage = () => {
   const { cartItems, totalAmount } = useCart();
@@ -10,14 +10,42 @@ const CartPage = () => {
         <Typography variant="body1">Your cart is empty</Typography>
       ) : (
         <>
-          {cartItems.map((item) => (
-            <div key={item.productId}>
-              <Typography variant="body1">{item.title}</Typography>
-              <Typography variant="body1">{item.quantity}</Typography>
-              <Typography variant="body1">{item.unitPrice}</Typography>
-            </div>
-          ))}
-          <Typography variant="body1">Total: {totalAmount}</Typography>
+          <Box display="flex" flexDirection="column" gap={4}>
+            {cartItems.map((item) => (
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                gap={2}
+                justifyContent="space-between"
+                sx={{ border: "1px solid #ccc", p: 2, borderRadius: 5 }}
+              >
+                <Box
+                  display="flex"
+                  flexDirection="row"
+                  alignItems="center"
+                  gap={2}
+                >
+                  <img src={item.image} alt={item.title} width={100} />
+                  <Box>
+                    <Typography variant="h6">{item.title}</Typography>
+                    <Typography>
+                      {item.quantity} X {item.unitPrice} $
+                    </Typography>
+                    <Button>Remove Item</Button>
+                  </Box>
+                </Box>
+                <ButtonGroup
+                  variant="contained"
+                  aria-label="Basic outlined button group"
+                >
+                  <Button>-</Button>
+                  <Button>+</Button>
+                </ButtonGroup>
+              </Box>
+            ))}
+          </Box>
+          <Typography variant="body1">Total: {totalAmount} $</Typography>
         </>
       )}
     </Container>
